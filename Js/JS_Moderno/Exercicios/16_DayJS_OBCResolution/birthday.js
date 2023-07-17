08bad351d5cc74ad4d40e4bdb8a4ac1a1cf86c93
André Euflauzino <andre.euflauzino@gmail.com>
2023-07-18 00:39:51 +0100

    exec16 - dayjs - OBC resolution

---
+++ Js/JS_Moderno/Exercicios/16_DayJS_OBCResolution/birthday.js
@@ -0,0 +1,18 @@
+const dayjs = require("dayjs")
+
+function birthday(date) {
+
+    const birthday = dayjs(date)
+    const today = dayjs()
+
+    const ageInYears = today.diff(birthday, 'y')
+    const nextBirthday = birthday.add(ageInYears + 1, 'y')
+    const daysToNextBirthday = nextBirthday.diff(today, 'd') + 1
+
+
+    console.log(`Idade: ${ageInYears} anos`);
+    console.log(`Próximo aniversário: ${nextBirthday.format("DD/MM/YYYY")}`);
+    console.log(`Faltam ${daysToNextBirthday} para completar ${ageInYears + 1} anos`);
+}
+
+birthday("1997-03-17")

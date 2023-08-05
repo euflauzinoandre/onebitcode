3408296504fb513d52cedb493e3d298c898a2633
André Euflauzino <andre.euflauzino@gmail.com>
2023-08-05 12:10:34 +0100

    POO-08-associacao-02

--- Js/POO/08-associacao/index.js
+++ Js/POO/08-associacao/index.js
@@ -1,8 +1,8 @@
-const Address = require("./Address");
+// const Address = require("./Address");
 const Person = require("./Person");
 
-const addr = new Address("Forest", 22, "Center", "Manhatan", "New York")
-const john = new Person("John doe", addr)
+// const addr = new Address("Forest", 22, "Center", "Manhatan", "New York")
+const john = new Person("John doe", "Forest", 22, "Center", "Manhatan", "New York")
 
 console.log(john)
-console.log(john.address.fullAddress());
+// console.log(john.address.fullAddress());
